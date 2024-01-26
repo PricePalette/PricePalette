e660268da8da11636da96200fd2ab4b53257d601
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-01-26 11:21:11 -0500

    setup material ui

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <AppCacheProvider {...props}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  );
 }
