77cfc694bceef74e0cb9999a617e2c1787609576
Shaik Luqmaan <luqmaanshaik@gmail.com>
2024-01-26 12:01:00 -0500

    Get Started Page (#1)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,21 +1,27 @@
-import { Container, Box, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme'; // Adjust the path accordingly
+import Header from '@/components/Header';
+
 
 export default function Home() {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js example in TypeScript
-        </Typography>
-      </Box>
-    </Container>
-  );
-}
+    <ThemeProvider theme={theme}>
+      <div>
+        <Header/>
+        <Container sx={{ marginTop: 4, textAlign: 'center' }}>
+          <div>
+            <Typography variant="h4" gutterBottom>
+              Price Pallette
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Customize price pages
+            </Typography>
+            <Button variant="contained" color="primary" size="large">
+              Get Started
+            </Button>
+          </div>
+        </Container>
+      </div>
+    </ThemeProvider>
+  );}
