1c000a333a6c72b15511bdcd6f8182e6181b6548
--global <pratik.choudhari89@gmail.com>
2024-02-02 21:29:05 -0500

    [UPDATE] take input from query parameters for widget router GET and DELETE requests

--- backend/widget_app/__init__.py
+++ backend/widget_app/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from pydantic.types import UUID4
 
 from backend.database import MONGO_CXN
 from backend.widget_app.models import UserID, WidgetID, WidgetMetadata
@@ -13,18 +14,19 @@ widget_router = APIRouter(
 
 
 @widget_router.get("/list")
-async def list_widgets(data: UserID):
-    widgets = widget_collection.find({"user_id": str(data.user_id)})
+async def list_widgets(user_id: UUID4):
+    widgets = widget_collection.find({"user_id": str(user_id)})
     if not widgets:
         return JSONResponse(content={"message": "error"}, status_code=404)
+    widgets = list(widgets)
     for widget in widgets:
         widget.pop("_id", None)
     return JSONResponse(content={"message": "OK", "content": widgets})
 
 
 @widget_router.get("/info")
-async def list_widgets(data: WidgetID):
-    widget = widget_collection.find_one({"widget_id": str(data.widget_id)})
+async def list_widgets(widget_id: UUID4):
+    widget = widget_collection.find_one({"widget_id": str(widget_id)})
     if not widget:
         return JSONResponse(content={"message": "error"}, status_code=404)
     widget.pop("_id", None)
@@ -40,14 +42,14 @@ async def list_widgets(data: WidgetMetadata):
 
 
 @widget_router.delete("/delete")
-async def list_widgets(data: WidgetID):
-    delete_result = widget_collection.delete_one({"widget_id": str(data.widget_id)})
+async def list_widgets(widget_id: UUID4):
+    delete_result = widget_collection.delete_one({"widget_id": str(widget_id)})
     if delete_result.deleted_count > 0:
         return JSONResponse(content={"message": "OK"})
     return JSONResponse(content={"message": "error"}, status_code=500)
 
 
 @widget_router.get("/embed")
-async def list_widgets(data: WidgetID):
+async def list_widgets(widget_id: UUID4):
     # embed information goes in SQL, write logic here
-    return JSONResponse(content={"message": "OK", "content": {"embed_id": data.widget_id}})
+    return JSONResponse(content={"message": "OK", "content": {"embed_id": widget_id}})
