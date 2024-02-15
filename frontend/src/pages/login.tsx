cd0a1afa05628c6e502d889ac423e1fe94286743
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-02-15 00:47:04 -0500

    login and signup with formik

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,8 +3,20 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import { useFormik } from "formik";
 
 export default function Login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, { setErrors }) => {
+      setErrors({ email: "change", password: "yo" });
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div
       style={{
@@ -27,18 +39,37 @@ export default function Login() {
           <Typography variant="h5" gutterBottom>
             Login
           </Typography>
-          <TextField
-            id="outlined-basic"
-            label="Username/email"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            type="password"
-          />
-          <Button variant="contained">Login</Button>
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={2}>
+              <TextField
+                id="email"
+                name="email"
+                label="Email"
+                type="email"
+                variant="outlined"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                id="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+              />
+              <Button variant="contained" type="submit">
+                Login
+              </Button>
+            </Stack>
+          </form>
           <Typography variant="subtitle2" gutterBottom>
             or
           </Typography>
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -3,8 +3,33 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import { useFormik } from "formik";
 
 export default function Register() {
+  const formik = useFormik({
+    initialValues: {
+      orgName: "",
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: (values, { setErrors }) => {
+      const { confirmPassword, password } = values;
+
+      // passwords dont match
+      if (confirmPassword !== password) {
+        setErrors({
+          password: "Passwords don't match",
+          confirmPassword: "Passwords don't match",
+        });
+        return;
+      }
+
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div
       style={{
@@ -27,26 +52,76 @@ export default function Register() {
           <Typography variant="h5" gutterBottom>
             Register
           </Typography>
-          <TextField
-            id="outlined-basic"
-            label="Organization Name"
-            variant="outlined"
-          />
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
-          <TextField id="outlined-basic" label="email" variant="outlined" />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            type="password"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Confirm Password"
-            variant="outlined"
-            type="password"
-          />
-          <Button variant="contained">Register</Button>
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={2}>
+              <TextField
+                id="orgName"
+                name="orgName"
+                label="Organization Name"
+                variant="outlined"
+                value={formik.values.orgName}
+                onChange={formik.handleChange}
+                error={formik.touched.orgName && Boolean(formik.errors.orgName)}
+                helperText={formik.touched.orgName && formik.errors.orgName}
+              />
+              <TextField
+                id="username"
+                name="username"
+                label="Username"
+                variant="outlined"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.username && Boolean(formik.errors.username)
+                }
+                helperText={formik.touched.username && formik.errors.username}
+              />
+              <TextField
+                id="email"
+                name="email"
+                label="email"
+                variant="outlined"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                id="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+              />
+              <TextField
+                id="confirmPassword"
+                name="confirmPassword"
+                label="Confirm Password"
+                variant="outlined"
+                type="password"
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.confirmPassword &&
+                  Boolean(formik.errors.confirmPassword)
+                }
+                helperText={
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
+                }
+              />
+              <Button variant="contained" type="submit">
+                Register
+              </Button>
+            </Stack>
+          </form>
           <Typography variant="subtitle2" gutterBottom>
             or
           </Typography>
