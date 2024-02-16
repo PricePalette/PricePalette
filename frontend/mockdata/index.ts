46aa52c3af3d333ec49d7992dd2deadb9414e025
samreethawilliams <samreethawilliams@gmail.com>
2024-02-16 01:09:32 -0500

    updated the getStarted page

--- frontend/mockdata/index.ts
+++ frontend/mockdata/index.ts
@@ -1,60 +1,57 @@
-export const templateData  = [
-    {
-        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-        title: 'Breakfast',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-
-      }
-
-]
+export const templateData = [
+  {
+    img: "/template1.png",
+    title: "Column View",
+  },
+  {
+    img: "/template1.png",
+    title: "Cards View",
+  },
+  {
+    img: "/template1.png",
+    title: "Vertical Cards View",
+  },
+  {
+    img: "/template1.png",
+    title: "Grid View",
+  },
+];
 
 export const plansData = [
   {
-    "id":"1",
-    "title":"Lite",
-    "subTitle":"Suitable for startups",
-    "price":'$15',
-    "features":[
-       "Access to all widgets",
-       "Usage upto 5000 views",
-       "User Friendly",
-       "100% Ad Free"
-    ]
- },
- {
-    "id":"2",
-    "title":"Pro",
-    "subTitle":"Suitable for small business",
-    "price":'$25',
-    "features":[
-       "Access to all widgets",
-       "Usage upto 15000 views",
-       "User Friendly",
-       "100% Ad Free"
-    ]
- },
- {
-    "id":"3",
-    "title":"Elite",
-    "subTitle":"Suitable for medium scale business",
-    "price":'$40',
-    "features":[
-       "Access to all widgets",
-       "Usage upto 30000 views",
-       "User Friendly",
-       "100% Ad Free"
-    ]
- }
-]
+    id: "1",
+    title: "Lite",
+    subTitle: "Suitable for startups",
+    price: "$15",
+    features: [
+      "Access to all widgets",
+      "Usage upto 5000 views",
+      "User Friendly",
+      "100% Ad Free",
+    ],
+  },
+  {
+    id: "2",
+    title: "Pro",
+    subTitle: "Suitable for small business",
+    price: "$25",
+    features: [
+      "Access to all widgets",
+      "Usage upto 15000 views",
+      "User Friendly",
+      "100% Ad Free",
+    ],
+  },
+  {
+    id: "3",
+    title: "Elite",
+    subTitle: "Suitable for medium scale business",
+    price: "$40",
+    features: [
+      "Access to all widgets",
+      "Usage upto 30000 views",
+      "User Friendly",
+      "100% Ad Free",
+    ],
+  },
+];
