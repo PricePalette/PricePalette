58292ff1e4e959109d15fb684cd3f5917a86c1e6
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-02-23 09:19:42 -0500

    [FIX] set sql pool size to 1

--- backend/database.py
+++ backend/database.py
@@ -7,4 +7,4 @@ from backend.configuration import MONGODB_CXN_STRING, MYSQL_CXN_STRING
 MONGO_CLIENT = MongoClient(MONGODB_CXN_STRING, server_api=ServerApi('1'))
 MONGO_CXN = MONGO_CLIENT["PricePalette"]
 
-ALCHEMY_ENGINE = create_engine(MYSQL_CXN_STRING)
+ALCHEMY_ENGINE = create_engine(MYSQL_CXN_STRING, pool_size=1)
