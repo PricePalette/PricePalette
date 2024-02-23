d409b04bce6033cbe7e16de3ab47b06a7ab5c17f
samreethawilliams <samreethawilliams@gmail.com>
2024-02-23 14:34:51 -0500

    Updated Header, Footer, FAQ

---
+++ frontend-v2/src/components/Footer/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Anchor, Group, ActionIcon, rem } from "@mantine/core";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
+import classes from "../../styles/footer.module.css";
+import { Logo } from "@/Logo";
+
+const links = [
+  { link: "#", label: "Contact Us" },
+  { link: "#", label: "Privacy Policy" },
+  { link: "#", label: "FAQ" },
+];
+
+export function Footer() {
+  const items = links.map((link) => (
+    <Anchor
+      c="dimmed"
+      key={link.label}
+      href={link.link}
+      lh={1}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <div className={classes.inner}>
+        <Logo height={40} width={100} />
+        <Group className={classes.links}>{items}</Group>
+
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+          <ActionIcon size="xl" variant="default" radius="xl">
+            <IconBrandTwitter
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandYoutube
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandInstagram
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        </Group>
+      </div>
+    </div>
+  );
+}
--- frontend-v2/src/pages/index.tsx
+++ frontend-v2/src/pages/index.tsx
@@ -1,13 +1,18 @@
 import { Button, Group, useMantineColorScheme } from "@mantine/core";
-
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 export default function IndexPage() {
   const { setColorScheme } = useMantineColorScheme();
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme("light")}>Light</Button>
-      <Button onClick={() => setColorScheme("dark")}>Dark</Button>
-      <Button onClick={() => setColorScheme("auto")}>Auto</Button>
-    </Group>
+    <>
+      <Header />
+      <Group justify="center" mt="xl">
+        <Button onClick={() => setColorScheme("light")}>Light</Button>
+        <Button onClick={() => setColorScheme("dark")}>Dark</Button>
+        <Button onClick={() => setColorScheme("auto")}>Auto</Button>
+      </Group>
+      <Footer />
+    </>
   );
 }
