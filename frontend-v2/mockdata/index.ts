c3cf38123f9ca28c80b66b6e80c049c74a73305c
Luqmaan Shaik <luqmaanshaik@gmail.com>
2024-02-24 01:21:21 -0500

    View Plan page in V2

---
+++ frontend-v2/mockdata/index.ts
@@ -0,0 +1,57 @@
+export const templateData = [
+  {
+    img: "/template1.png",
+    title: "Column View",
+  },
+  {
+    img: "/template1.png",
+    title: "Cards View",
+  },
+  {
+    img: "/template1.png",
+    title: "Vertical Cards View",
+  },
+  {
+    img: "/template1.png",
+    title: "Grid View",
+  },
+];
+
+export const plansData = [
+  {
+    id: "1",
+    title: "Lite",
+    subTitle: "Suitable for startups",
+    price: "$15",
+    features: [
+      "Access to all widgets",
+      "Usage upto 5000 views",
+      "User Friendly",
+      "100% Ad Free",
+    ],
+  },
+  {
+    id: "2",
+    title: "Pro",
+    subTitle: "Suitable for small business",
+    price: "$25",
+    features: [
+      "Access to all widgets",
+      "Usage upto 15000 views",
+      "User Friendly",
+      "100% Ad Free",
+    ],
+  },
+  {
+    id: "3",
+    title: "Elite",
+    subTitle: "Suitable for medium scale business",
+    price: "$40",
+    features: [
+      "Access to all widgets",
+      "Usage upto 30000 views",
+      "User Friendly",
+      "100% Ad Free",
+    ],
+  },
+];
