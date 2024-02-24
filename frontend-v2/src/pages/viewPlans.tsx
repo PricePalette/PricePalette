7076c22c7675617921c606d395a3912d809159fd
Luqmaan Shaik <luqmaanshaik@gmail.com>
2024-02-24 01:41:04 -0500

    Integrate Header and Footer

--- frontend-v2/src/pages/viewPlans.tsx
+++ frontend-v2/src/pages/viewPlans.tsx
@@ -2,37 +2,48 @@ import React from 'react';
 import { Card, Text, Button, Group, Container, List, ListItem } from '@mantine/core';
 import { plansData } from '../../mockdata';
 import { IconCheck } from '@tabler/icons-react';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 export default function ViewPlans() {
-  return (
-    // <Header/>
-    <Container size="sm" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '40px' }}>
-        {plansData.map((eachPlan, index) => (
-          <Card key={index} shadow="lg" radius="md" style={{ width: 300, height: 440, padding: 18 }}>
-            <Card.Section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', gap: '10px' }}>
-              <Text size="lg" fw={500}>{eachPlan.title}</Text>
-              <Text size="md" style={{ marginBottom: '10px' }}>{eachPlan.subTitle}</Text>
-              <Text fw={700} style={{ fontSize: 26, lineHeight: 1, marginBottom: '20px' }}>{eachPlan.price}</Text>
-            </Card.Section>
-
-            <Group mt="md" mb="xs" style={{ alignItems: 'flex-start' }}>
-              <List style={{ marginTop: '8px' }}>
-                {eachPlan.features.map((eachFeature, index) => (
-                  <ListItem key={index} icon={<IconCheck size={18} />} style={{ marginBottom: '8px' }}>
-                    {eachFeature}
-                  </ListItem>
-                ))}
-              </List>
-            </Group>
-
-            <Button variant="light" color="blue" mt="md" radius="md">
-              Buy now
-            </Button>
-          </Card>
-        ))}
-      </div>
-    </Container>
-    // <Footer/>
-  );
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Header />
+          <Container size="sm" style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '40px' }}>
+              {plansData.map((eachPlan, index) => (
+                <Card key={index} shadow="lg" radius="md" style={{ width: 300, height: 440}}>
+                  <Card.Section style={{ 
+                    display: 'flex', 
+                    flexDirection: 'column', 
+                    alignItems: 'center', 
+                    textAlign: 'center', 
+                    gap: '10px',
+                    padding: '18px'  
+                  }}>
+                    <Text size="lg" fw={500}>{eachPlan.title}</Text>
+                    <Text size="md" style={{ marginBottom: '10px' }}>{eachPlan.subTitle}</Text>
+                    <Text fw={700} style={{ fontSize: 26, lineHeight: 1, }}>{eachPlan.price}</Text>
+                  </Card.Section>
+    
+                  <Group mt="md" mb="xs" style={{ alignItems: 'flex-start' }}>
+                    <List style={{ marginTop: '8px' }}>
+                      {eachPlan.features.map((eachFeature, index) => (
+                        <ListItem key={index} icon={<IconCheck size={18} />} style={{ marginBottom: '8px' }}>
+                          {eachFeature}
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Group>
+    
+                  <Button variant="light" color="blue"  radius="md" style={{marginTop: '30px'}} >
+                    Buy now
+                  </Button>
+                </Card>
+              ))}
+            </div>
+          </Container>
+          <Footer />
+        </div>
+      );
 }
