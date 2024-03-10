d307cd082d9fdf528b6ef3b846189ae39dd0eb7f
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-03-09 21:37:53 -0500

    rm -rf frontend (#37)

--- frontend/src/utils/toErrorMap.ts
+++
@@ -1,9 +0,0 @@
-export const toErrorMap = (errors: { field: string; message: string }[]) => {
-  const errorMap: Record<string, string> = {};
-
-  errors.forEach(({ field, message }) => {
-    errorMap[field] = message;
-  });
-
-  return errorMap;
-};
