0971bbae25e0e9bccc2ff08a8f1a5b069ec92e80
samreethawilliams <samreethawilliams@gmail.com>
2024-03-30 16:26:55 -0400

    template list api integrated

--- frontend-v2/src/components/DynamicTemplateLoader/index.tsx
+++ frontend-v2/src/components/DynamicTemplateLoader/index.tsx
@@ -1,18 +1,18 @@
 import { GridTemplate } from "@/templates/GridTemplate";
 
 interface DynamicTemplateLoaderProps {
-  id: number;
+  id: string;
 }
 
 const DynamicTemplateLoader: React.FC<DynamicTemplateLoaderProps> = ({
   id,
 }) => {
   switch (id) {
-    case 1:
+    case "21c86e6a-eac0-4278-8fb4-30e80bb23026":
       return <GridTemplate />;
-    case 2:
+    case " 2":
       return <GridTemplate />;
-    case 3:
+    case "3":
       return <GridTemplate />;
     default:
       return <p>No component found for this ID.</p>;
