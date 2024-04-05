129f36096d65556a66c09b9ea65f51751b00d8da
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-04-04 21:44:16 -0400

    [FIX] renew mail gun api key

--- backend/configuration.py
+++ backend/configuration.py
@@ -4,8 +4,9 @@ from urllib.request import urlretrieve
 
 from dotenv import load_dotenv
 
-blob_url = ("https://pricepalettefiles.blob.core.windows.net/backend/env?sp=r&st=2024-03-09T23:49:49Z"
-            f"&se=2024-07-01T05:49:49Z&sv=2022-11-02&sr=b&sig={os.environ['BLOB_SIGNATURE']}")
+blob_url = """https://pricepalettefiles.blob.core.windows.net/backend/env?sp=r&st=2024-04-05T01:40:22Z""" \
+            """&se=2024-05-31T09:40:22Z&sip=0.0.0.0-255.255.255.255&spr=https&sv=2022-11-02&sr=b""" \
+            f"""&sig={os.environ['BLOB_SIGNATURE']}"""
 
 urlretrieved = urlretrieve(blob_url)
 with open(urlretrieved[0], "r") as fp:
@@ -16,8 +17,8 @@ PRICEPALETTE_PLANS = {"price_1Ov0bVCjcrhrZTSatO9LbWyF": 5000,
                       "price_1Ov0bVCjcrhrZTSaNA5k670V": 30000,
                       "631e263e-d9ae-40cc-ac21-91d71fe7c9fd": 50}
 
-MG_KEY = '4978b1ef6df5b97264b6176be2aa16dc-2c441066-291227e7'
-MG_SANDBOX = 'mg.pricepalette.tech'
+MG_KEY = os.environ["MG_KEY"]
+MG_SANDBOX = os.environ["MG_SANDBOX"]
 
 JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
 JWT_ALGORITHM = os.environ["JWT_ALGORITHM"]
